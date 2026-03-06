75d063e36ff692713db587eb9302f295a93bda35
xiaoheimaoo <xiaoheimaoo@users.noreply.github.com>
2026-03-06 02:41:27 +0000

    Update Geetest version to v1.9.5-061a98

--- geeked/sign.py
+++ geeked/sign.py
@@ -16,7 +16,7 @@ from geeked.icon import IconSolver
 
 class LotParser:
     def __init__(self):
-        self.mapping = {"n[15:18]+n[1:4]":'n[17:22]'}
+        self.mapping = {"(n[1:8])+.+(n[28:28]+n[24:24]+n[14:14]+n[15:15])":'n[18:21]'}
         self.lot = []
         self.lot_res = []
         for k, v in self.mapping.items():
@@ -181,7 +181,7 @@ function encrypt_asymmetric_2(input, key) {
     def generate_w(data: dict, captcha_id: str, risk_type: str):
         lot_number = data['lot_number']
         pow_detail = data['pow_detail']
-        abo = {"S59M":"rwPC"}
+        abo = {"i1ou":"TXEY"}
         base = abo | {
             **Signer.generate_pow(lot_number, captcha_id, pow_detail['hashfunc'], pow_detail['version'],
                                   pow_detail['bits'], pow_detail['datetime'], ""),
